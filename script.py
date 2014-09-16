76dc174a80b62ef4f57e97012a10863088e635cf
Edward <edwardkt@usc.edu>
2014-09-16 23:06:26 +0000

    modified connection

--- script.py
+++ script.py
@@ -2,4 +2,5 @@
 
 import boto.ec2
 
-conn = boto.ec2.connect_to_region("us-west-2",aws_access_key_id='<aws access key>',aws_secret_access_key='<aws secret key>')
+ec2 = boto.ec2.connect_to_region('us-west-2')
+
